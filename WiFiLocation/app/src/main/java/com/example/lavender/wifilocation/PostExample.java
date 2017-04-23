4c47b8c725c9d628dd20fdfeb7e3c01177837cc8
lavender21 <1573872488@qq.com>
2017-04-23 17:46:37 +0800

    Add okHttp demoo

---
+++ WiFiLocation/app/src/main/java/com/example/lavender/wifilocation/PostExample.java
@@ -0,0 +1,31 @@
+package com.example.lavender.wifilocation;
+
+import java.io.IOException;
+
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+/**
+ * Created by lavender on 2017/4/23.
+ */
+
+public class PostExample {
+    public static final MediaType JSON
+            = MediaType.parse("application/json; charset=utf-8");
+
+    OkHttpClient client = new OkHttpClient();
+
+    String post(String url, String json) throws IOException {
+        RequestBody body = RequestBody.create(JSON, json);
+        Request request = new Request.Builder()
+                .url(url)
+                .post(body)
+                .build();
+        Response response = client.newCall(request).execute();
+        return response.body().string();
+    }
+
+}
