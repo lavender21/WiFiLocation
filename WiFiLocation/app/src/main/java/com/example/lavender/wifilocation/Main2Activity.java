18bc834ad9d21907c54f8265066e229ce9caaa98
lavender21 <1573872488@qq.com>
2017-05-05 15:49:22 +0800

    Add location algorithm spinner in app

--- WiFiLocation/app/src/main/java/com/example/lavender/wifilocation/Main2Activity.java
+++ WiFiLocation/app/src/main/java/com/example/lavender/wifilocation/Main2Activity.java
@@ -26,13 +26,14 @@ import static com.example.lavender.wifilocation.Common.toJson;
 
 
 public class Main2Activity extends AppCompatActivity implements View.OnClickListener {
-    private Button btnLocation,btnScan,btnCollection;
+    private Button btnLocation, btnScan, btnCollection;
     private String apData;
-    private TextView showData,txtCoord,txtRoom;
+    private TextView showData, txtCoord, txtRoom,txtAlgorithm;
     private EditText edtActualCoord;
-    private Spinner spinner;
-    private String room_id;
-    private String[] data = new String[]{"科协办公室","实验室1","实验室2"};
+    private Spinner spinner, spinnerAlgorithm;
+    private String room_id, algorithm;
+    private String[] data = new String[]{"科协办公室", "实验室1", "实验室2"};
+    private String[] data2 = new String[]{"knn", "模糊集"};
 
     private BroadcastReceiver receiver = new BroadcastReceiver() {
         @Override
@@ -41,12 +42,13 @@ public class Main2Activity extends AppCompatActivity implements View.OnClickList
             showData.setText(apData);
         }
     };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
         init();
-        registerReceiver(receiver,new IntentFilter("apData"));
+        registerReceiver(receiver, new IntentFilter("apData"));
     }
 
     @Override
@@ -55,25 +57,45 @@ public class Main2Activity extends AppCompatActivity implements View.OnClickList
         super.onDestroy();
     }
 
-    private void init()
-    {
-        btnLocation = (Button)findViewById(R.id.btnLocation);
+    private void init() {
+        btnLocation = (Button) findViewById(R.id.btnLocation);
         btnLocation.setOnClickListener(this);
-        btnScan = (Button)findViewById(R.id.btnScan);
+        btnLocation.setEnabled(false);
+        btnScan = (Button) findViewById(R.id.btnScan);
         btnScan.setOnClickListener(this);
-        btnCollection = (Button)findViewById(R.id.btnCollection);
+        btnCollection = (Button) findViewById(R.id.btnCollection);
         btnCollection.setOnClickListener(this);
-        showData = (TextView)findViewById(R.id.showRssiData);
-        txtCoord = (TextView)findViewById(R.id.textCoord);
-        txtRoom = (TextView)findViewById(R.id.txtRoom);
-        edtActualCoord = (EditText)findViewById(R.id.edtActualCoord);
-        spinner = (Spinner)findViewById(R.id.spinner);
-        spinner.setAdapter(new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,data));
+        showData = (TextView) findViewById(R.id.showRssiData);
+        txtCoord = (TextView) findViewById(R.id.textCoord);
+        txtRoom = (TextView) findViewById(R.id.txtRoom);
+        txtAlgorithm = (TextView)findViewById(R.id.txtAlgorithm);
+        edtActualCoord = (EditText) findViewById(R.id.edtActualCoord);
+        spinner = (Spinner) findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.room_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 room_id = id + 1 + "";
             }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+        spinnerAlgorithm = (Spinner) findViewById(R.id.spinnerAlogrithm);
+        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this,
+                R.array.algorithm_array, android.R.layout.simple_spinner_item);
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerAlgorithm.setAdapter(adapter2);
+        spinnerAlgorithm.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                algorithm = id + "";
+            }
+
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
             }
@@ -82,14 +104,17 @@ public class Main2Activity extends AppCompatActivity implements View.OnClickList
 
     @Override
     public void onClick(View v) {
-        switch (v.getId())
-        {
+        switch (v.getId()) {
             case R.id.btnScan:
+                btnLocation.setEnabled(true);
+                btnScan.setEnabled(false);
                 Intent intent = new Intent(Main2Activity.this, GetRSSIService.class);
                 startService(intent);
                 break;
             case R.id.btnLocation:
                 locationPost();
+                btnLocation.setEnabled(false);
+                btnScan.setEnabled(true);
                 break;
             case R.id.btnCollection:
                 Intent intent1 = new Intent(Main2Activity.this, GetRssiActivity.class);
@@ -97,20 +122,18 @@ public class Main2Activity extends AppCompatActivity implements View.OnClickList
         }
     }
 
-    private void locationPost()
-    {
-        Intent intent = new Intent(Main2Activity.this,GetRSSIService.class);
+    private void locationPost() {
+        Intent intent = new Intent(Main2Activity.this, GetRSSIService.class);
         stopService(intent);
 
         JSONObject json = new JSONObject();
-        try
-        {
-            json.put("room_id",room_id);
-            json.put("mobile_id","3");
-            json.put("actual_coord",edtActualCoord.getText());
-            json.put("ap",toJson(apData));
-        }catch (JSONException e)
-        {
+        try {
+            json.put("room_id", room_id);
+            json.put("mobile_id", "3");
+            json.put("actual_coord", edtActualCoord.getText());
+            json.put("algorithm", algorithm);
+            json.put("ap", toJson(apData));
+        } catch (JSONException e) {
             e.printStackTrace();
         }
 
@@ -120,6 +143,6 @@ public class Main2Activity extends AppCompatActivity implements View.OnClickList
                 txtCoord.setText("您的当前位置为：" + output);
             }
         });
-        http.execute("POST",http.LOCATION,json.toString());
+        http.execute("POST", http.LOCATION, json.toString());
     }
 }
