47406263760a20ae19ba4fa00595cf9734f43b1f
lavender21 <1573872488@qq.com>
2017-05-17 17:03:01 +0800

    Modify DataTest controller for generate sensor data

--- WiFiLocationServer/WiFiLocationServer/Controllers/DataTestController.cs
+++ WiFiLocationServer/WiFiLocationServer/Controllers/DataTestController.cs
@@ -21,23 +21,47 @@ namespace WiFiLocationServer.Controllers
 
         // Get /api/DataTest/id
         [HttpGet]
-        public DataSet GetById(int id)
+        public Models.data_test GetById(int id)
         {
-            //Models.data_test model = db.GetModel(id);
-            //dataset ds = rssidb.getrssilist("room_id = " + id + " and mobile_id = 3 and mac in ('0a:69:6c:51:36:80','0a:69:6c:51:36:7f')");
-            DataSet ds = fingerDb.getCoordList(Convert.ToString(id));
+            Models.data_test model = db.GetModel(id);
             var response = Request.CreateResponse();
-            if (ds != null)
+            if (model != null)
             {
                 response = Request.CreateResponse(HttpStatusCode.OK);
-                response.Content = new StringContent(ds.ToString(), Encoding.Unicode);
+                response.Content = new StringContent(model.ToString(), Encoding.Unicode);
             }
             else
             {
                 response = Request.CreateResponse(HttpStatusCode.NotFound);
                 response.Content = new StringContent("{\"message\":\"无该资源\"}", Encoding.Unicode);
             }
-            return ds;
+            return model;
+        }
+
+        // POST /api/DataTest/
+        public HttpResponseMessage Post([FromBody]Models.data_test model)
+        {
+            var response = Request.CreateResponse();
+
+            if (model != null)
+            {
+                if (db.Add(model) > 0)
+                {
+                    response = Request.CreateResponse(HttpStatusCode.OK);
+                    response.Content = new StringContent("{\"message\":\"添加成功\"}", Encoding.UTF8);
+                }
+                else
+                {
+                    response = Request.CreateResponse(HttpStatusCode.InternalServerError);
+                    response.Content = new StringContent("{\"message\":\"发生未知错误\"}", Encoding.UTF8);
+                }
+            }
+            else
+            {
+                response = Request.CreateResponse(HttpStatusCode.BadRequest);
+                response.Content = new StringContent("{\"message\":\"post数据为空\"}", Encoding.Unicode);
+            }
+            return response;
         }
 
     }
